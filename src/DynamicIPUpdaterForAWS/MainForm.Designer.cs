00e5bbbd22e6c04737d546fa84621efd9acaa8f5
ahmed mohamed zeyada <ahmed_zeyad@hotmail.com>
2021-06-29 19:32:47 +0200

    hide notifyIcon in startup

--- src/DynamicIPUpdaterForAWS/MainForm.Designer.cs
+++ src/DynamicIPUpdaterForAWS/MainForm.Designer.cs
@@ -98,7 +98,6 @@
             this.notifyIcon1.BalloonTipTitle = "Dynamic IP Updater";
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "Dynamic IP Updater";
-            this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
             // MainForm
